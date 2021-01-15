c64f8c13354c2a66ff06e5327c671b945bed4bf5
Martina <73060669+martina0911@users.noreply.github.com>
2021-01-15 21:05:11 +0100

    Update vjezbe9.c

--- Vjezbe9/vjezbe9.c
+++ Vjezbe9/vjezbe9.c
@@ -3,155 +3,186 @@ Treba omogućiti unošenje novog elementa u stablo, ispis elemenata, brisanje i 
 
 
 #define _CRT_SECURE_NO_WARNINGS
-
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-struct _node;
-typedef struct _node* Position;
-typedef struct _node
+struct node;
+typedef struct node* Pozicija;
+typedef struct node
 {
-	int number;
-	Position left;
-	Position right;
+	int El;
+	Pozicija Right;
+	Pozicija Left;
 }Node;
 
-Position insert(Position current, Position El);
-Position find(Position current, int number);
-void printInOrder(Position current);
-Position deleteNode(Position current, int number);
-Position findMax(Position current);
-Position findMin(Position current);
+Pozicija Insert(Pozicija, int);
+void Print(Pozicija);
+Pozicija FindMin(Pozicija);
+Pozicija FindMax(Pozicija);
+Pozicija Delete(Pozicija, int);
+Pozicija Find(Pozicija, int);
 
 int main()
 {
-	Position root = NULL;
-  root = (Position) malloc (sizeof (Node));
-	Position El;
-  int selection = 1;
-  
-  while (selection)
-  {
-    printf("\nIzaberite broj:\n);
-    printf("\n1-Unesite element u stablo\n2-Pronadite element\n3-Izbrisi\n4-Ispis\n\n);
-    scanf("%d ", &selection);
-           switch(selection)
-           {
-             case 1:
-                     {
-                      
-  
+	Pozicija root = NULL;
+	Pozicija find = NULL;
+	char izbor = '0';
+	int a = 0;
+    while(izbor!='z')
+	{
+		printf("\nOdaberite: \n");
+		printf("1.- Unos\n");
+		printf("2.- Ispis\n");
+		printf("3.- Brisanje\n");
+		printf("4.- Trazenje\n");
+		printf("Vas izbor je: \n");
+		scanf(" %c", &izbor);
+		switch (izbor)
+		{
+		case '1':
+		{
+			printf("\nUnesite broj koji zelite dodati: ");
+			scanf(" %d", &a);
+			root = Insert(root, a);
+			break;
+		}
+		case '2':
+		{
+			if (root == NULL)
+				printf("\nStablo je prazno");
+			else
+				Print(root);
+			break;
+		}
+		case '3':
+		{
+			printf("\nUnesite broj koji zelite izbrisati : ");
+			scanf(" %d", &a);
+			root = Delete(root, a);
+			break;
+		}
+		case '4':
+		{
+			printf("\nUnesite broj koji trazite : ");
+			scanf(" %d", &a);
+			find = Find(root, a);
+			if (find != NULL)
+				printf("\nTrazeni element %d se nalazi na adresi %x", find->El, find);
+			else
+				printf("\nTrazeni broj %d nije pronaden.", a);
+			break;
+		}
+		case 'z':
+		{
+			printf("Izlaz.\n");
+			break;
+		}
+		default:
+		{
+			printf("\nPogresan izbor!\nPokusajte ponovno.\n");
+		}
+		}
+	}
 	return 0;
 }
 
-Position insert(Position current, Position El)
+Pozicija Insert(Pozicija P, int n)
 {
-	if (NULL == current)
-		return El;
-	if (current->number > El->number)
+	if (P == NULL)
 	{
-		current->left = insert(current->left, El);
-	}
-	else if (current-> number < El->number)
-	{
-		current->right = insert(current->right, El);
-	}
-	else
-	{
-		free(El);
-	}
-
-	return current;
-}
-
-Position find(Position current, int number)
-{
-	if (NULL == current)
-		return NULL;
-
-	if (current->number > number)
-	{
-		return find(current->left, number);
-	}
-	else if (current->number < number)
-	{
-		return find(current->right, number);
-	}
-	else
-    return current;
-}
-
-void printInOrder(Position current)
-{
-	if (NULL == current)
-		return;
-
-	printInOrder(current->left);
-	printf("%d ", current->number);
-	printInOrder(current->right);
-
-}
-
-Position deleteNode(Position current, int number)
-{
-	if (NULL == current)
-		return NULL;
-
-	if (current->number == number)
-	{
-		/*if (current->left == NULL && current->right == NULL)
+		P = (Pozicija)malloc(sizeof(Node));
+		if (P)
 		{
-			free(current);
-			return NULL;
-		}*/
-		
-		if (current->left != NULL)
-		{
-			Position tmp = findMax(current->left);
-			current->number = tmp->number;
-			current->left = deleteNode(current->left, tmp->number);
+			P->El = n;
+			P->Left = NULL;
+			P->Right = NULL;
 		}
-		else if (current->right != NULL)
+	}
+	else if (P->El < n)
+		P->Right = Insert(P->Right, n);
+	else if (P->El > n)
+		P->Left = Insert(P->Left, n);
+	else
+		printf("\nElement vec postoji u stablu.");
+	return P;
+}
+
+void Print(Pozicija P)
+{
+	if (P != NULL)
+	{
+		Print(P->Left);
+		printf(" %d", P->El);
+		Print(P->Right);
+	}
+}
+
+Pozicija FindMin(Pozicija P)
+{
+	if (P == NULL)
+		return P;
+	else if (P->Left == NULL)
+		return P;
+	else
+		return FindMin(P->Left);
+}
+
+Pozicija FindMax(Pozicija P)
+{
+	if (P != NULL)
+	{
+		while (P->Right != NULL)
+			P = P->Right;
+	}
+	return P;
+}
+
+Pozicija Find(Pozicija P, int n)
+{
+	if (P == NULL)
+		return NULL;
+	else
+		if (P->El > n)
+			return Find(P->Left, n);
+		else if (P->El < n)
+			return Find(P->Right, n);
+		else
+			return P;
+}
+
+Pozicija Delete(Pozicija P, int n)
+{
+	Pozicija tmp;
+	if (P == NULL)
+		printf("\nElement %d nije pronaden.", n);
+	else if (n < P->El)
+		P->Left = Delete(P->Left, n);
+	else if (n > P->El)
+		P->Right = Delete(P->Right, n);
+	else
+	{
+		if (P->Left != NULL && P->Right != NULL)
 		{
-			Position tmp = findMin(current->right);
-			current->number = tmp->number;
-			current->right = deleteNode(current->right, tmp->number);
+			//trazimo najveci element u lijevom podstablu ili najmanji u desnom
+			tmp = FindMin(P->Right);
+			P->El = tmp->El;
+			P->Right = Delete(P->Right, tmp->El);
+			/*tmp = FindMax(P->Left); tr
+			P->El = tmp->El;
+			P->Left = Delete(P->Left, tmp->El);*/
+			
 		}
 		else
 		{
-			free(current);
-			return NULL;
+			tmp = P;
+			if (P->Left == NULL)
+				P = P->Right;
+			else
+				P = P->Left;
+			free(tmp);
 		}
 	}
-	else if (current->number > number)
-	{
-		current->left = deleteNode(current->left, number);
-	}
-	else //if (current->number < number)
-	{
-		current->right = deleteNode(current->right, number);
-	}
 
-  return current;
-}
-
-Position findMax(Position current) 
-{
-	if (NULL == current)
-		return NULL;
-
-	while (current->right != NULL)
-		current = current->right;
-
-	return current;
-}
-Position findMin(Position current)
-{
-	if (NULL == current)
-		return NULL;
-
-	while (current->left != NULL)
-		current = current->left;
-
-	return current;
+	return P;
 }
